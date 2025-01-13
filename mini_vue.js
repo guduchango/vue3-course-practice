be38be3a40c0b3e7f5dc69282eba5caa3114b9a1
Edgardo Ponce <ed.poncepaez@gmail.com>
2025-01-13 16:44:11 -0300

    adding trigger and track method to the minivue app

--- mini_vue.js
+++ mini_vue.js
@@ -1,36 +1,75 @@
 class MiniVueReactive {
+  // Dependencias
+  deps = new Map();
+
   /*
     options:
       data() => { ... }
   */
-      constructor({ data }) {
-        this.origen = data();
-    
-        // Destino
-        this.$data = new Proxy(this.origen, {
-          get(target, name) {
-            if (name in target) {
-              return target[name];
-            }
-            console.warn("La propiedad", name, "no existe");
-            return "";
-          },
-          set() {}
+  constructor({ data }) {
+    this.origen = data();
+
+    const self = this;
+
+    // Destino
+    this.$data = new Proxy(this.origen, {
+      get(target, name) {
+        /* if (name in target) {
+          return target[name];
+        } */
+        if (Reflect.has(target, name)) {
+          self.track(target, name);
+          return Reflect.get(target, name);
+        }
+        console.warn("La propiedad", name, "no existe");
+        return "";
+      },
+      set(target, name, value) {
+        Reflect.set(target, name, value);
+        self.trigger(name);
+      }
+    });
+  }
+
+  track(target, name) {
+    if (!this.deps.has(name)) {
+      const effect = () => {
+        document.querySelectorAll(`*[p-text=${name}]`).forEach(el => {
+          this.pText(el, target, name);
         });
-      }
-    
-      mount() {
-        document.querySelectorAll("*[p-text]").forEach(el => {
-          this.pText(el, this.$data, el.getAttribute("p-text"));
-        });
-      }
-    
-      pText(el, target, name) {
-        el.innerText = target[name];
-      }
-    
-      pModel() {}
+      };
+      this.deps.set(name, effect);
+    }
+  }
+  trigger(name) {
+    const effect = this.deps.get(name);
+    effect();
+  }
+
+  mount() {
+    document.querySelectorAll("*[p-text]").forEach(el => {
+      this.pText(el, this.$data, el.getAttribute("p-text"));
+    });
+
+    document.querySelectorAll("*[p-model]").forEach(el => {
+      const name = el.getAttribute("p-model");
+      this.pModel(el, this.$data, name);
+
+      el.addEventListener("input", () => {
+        Reflect.set(this.$data, name, el.value);
+      });
+    });
+  }
+
+  pText(el, target, name) {
+    el.innerText = Reflect.get(target, name);
+  }
+
+  pModel(el, target, name) {
+    el.value = Reflect.get(target, name);
+  }
 }
+
 
 var MiniVue = {
   createApp(options) {

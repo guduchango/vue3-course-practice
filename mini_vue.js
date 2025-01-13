20d0025397632145d3290d4c0e826f34128e70f3
Edgardo Ponce <ed.poncepaez@gmail.com>
2025-01-13 15:07:32 -0300

    adding proxi class get, to filter accions does i want to do

--- mini_vue.js
+++ mini_vue.js
@@ -3,21 +3,33 @@ class MiniVueReactive {
     options:
       data() => { ... }
   */
-  constructor({ data }) {
-    this.origen = data();
-  }
-
-  mount() {
-    document.querySelectorAll("*[p-text]").forEach(el => {
-      this.pText(el, this.origen, el.getAttribute("p-text"));
-    });
-  }
-
-  pText(el, target, name) {
-    el.innerText = target[name];
-  }
-
-  pModel() {}
+      constructor({ data }) {
+        this.origen = data();
+    
+        // Destino
+        this.$data = new Proxy(this.origen, {
+          get(target, name) {
+            if (name in target) {
+              return target[name];
+            }
+            console.warn("La propiedad", name, "no existe");
+            return "";
+          },
+          set() {}
+        });
+      }
+    
+      mount() {
+        document.querySelectorAll("*[p-text]").forEach(el => {
+          this.pText(el, this.$data, el.getAttribute("p-text"));
+        });
+      }
+    
+      pText(el, target, name) {
+        el.innerText = target[name];
+      }
+    
+      pModel() {}
 }
 
 var MiniVue = {
